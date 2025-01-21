dcbe0c771db1fd5ab2831772048527094883bf8a
Sabbir <bsse0728@iit.du.ac.bd>
2025-01-21 17:01:34 +0600

    Adding build version related info in Customer Module

--- ecommerce-services/customer/src/main/java/com/sabbir/customer/CustomerApplication.java
+++ ecommerce-services/customer/src/main/java/com/sabbir/customer/CustomerApplication.java
@@ -1,11 +1,14 @@
 package com.sabbir.customer;
 
+import com.sabbir.customer.model.dto.CustomerContactInfoDto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {CustomerContactInfoDto.class})
 public class CustomerApplication {
 
 	public static void main(String[] args) {
--- ecommerce-services/customer/src/main/java/com/sabbir/customer/controller/CustomerController.java
+++ ecommerce-services/customer/src/main/java/com/sabbir/customer/controller/CustomerController.java
@@ -1,11 +1,15 @@
 package com.sabbir.customer.controller;
 
+import com.sabbir.customer.model.dto.CustomerContactInfoDto;
 import com.sabbir.customer.model.dto.CustomerDto;
 import com.sabbir.customer.service.CustomerService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
@@ -23,6 +27,15 @@ public class CustomerController {
     public CustomerController(CustomerService customerService) {
         this.customerService = customerService;
     }
+
+    @Value("${build.version}")
+    private String buildVersion;
+
+    @Autowired
+    private Environment env;
+
+    @Autowired
+    private CustomerContactInfoDto customerContactInfoDto;
 
     @PostMapping("/createCustomer")
     public ResponseEntity<Long> createCustomer(@Valid @RequestBody CustomerDto customerDto) {
@@ -53,6 +66,27 @@ public class CustomerController {
     @GetMapping("/fetchCustomer/mobileNumber")
     public ResponseEntity<CustomerDto> fetchCustomerByMobileNumber(@RequestParam @Pattern(regexp = "[0-9]{11}") String mobileNumber) {
         return ResponseEntity.status(HttpStatus.OK).body(customerService.fetchCustomer(mobileNumber));
+    }
+
+
+    @GetMapping("/build-info")
+    public ResponseEntity<String > getBuildInfo() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(buildVersion);
+    }
+    @GetMapping("/java-info")
+    public ResponseEntity<String> getJavaVersion() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(env.getProperty("JAVA_HOME"));
+    }
+
+    @GetMapping("contact-info")
+    public ResponseEntity<CustomerContactInfoDto> getContactInfo() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(customerContactInfoDto);
     }
 
 }
