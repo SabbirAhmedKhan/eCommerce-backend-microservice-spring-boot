daa3ee1386a2af700effb7edce4591b5d18876b3
Sabbir <bsse0728@iit.du.ac.bd>
2025-01-23 16:49:22 +0600

    Skeleton of Order microservice

--- ecommerce-services/order/src/main/java/com/sabbir/order/OrderApplication.java
+++ ecommerce-services/order/src/main/java/com/sabbir/order/OrderApplication.java
@@ -1,13 +1,13 @@
-package com.sabbir.customer;
+package com.sabbir.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CustomerApplication {
+public class OrderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CustomerApplication.class, args);
+		SpringApplication.run(OrderApplication.class, args);
 	}
 
 }
--- ecommerce-services/order/src/test/java/com/sabbir/order/OrderApplicationTests.java
+++ ecommerce-services/order/src/test/java/com/sabbir/order/OrderApplicationTests.java
@@ -1,10 +1,10 @@
-package com.sabbir.customer;
+package com.sabbir.order;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CustomerApplicationTests {
+class OrderApplicationTests {
 
 	@Test
 	void contextLoads() {
